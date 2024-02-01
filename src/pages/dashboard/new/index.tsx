c6762109e494e4142d55ef116f3c258e45fd6ef5
felipeCast <castfelipe10@gmail.com>
2024-02-01 11:22:49 -0300

    Adicionando funcionalidade de cadastrar um carro

--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -1,66 +1,74 @@
 import { ChangeEvent, useState, useContext } from "react";
 import { Container } from "../../../components/container";
+import { FiUpload, FiTrash } from 'react-icons/fi'
 import { DashboardHeader } from "../../../components/panelHeader";
+import { useForm } from 'react-hook-form'
+import { Input } from '../../../components/input'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-import { FiUpload } from "react-icons/fi";
-import {useForm} from 'react-hook-form';
-import { Input } from "../../../components/input";
-import { z } from 'zod';
-import { zodResolver } from "@hookform/resolvers/zod";
+import { v4 as uuidV4 } from 'uuid'
 
+import { storage, db } from '../../../services/firebaseConnection'
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  deleteObject
+} from 'firebase/storage'
 import { AuthContext } from "../../../contexts/auth";
-import { storage } from "../../../services/firebaseConnection";
 
-import { ref, uploadBytes, getDownloadURL, deleteObject } from 'firebase/storage';
+import { addDoc, collection, } from 'firebase/firestore';
 
-import { v4 as uuidV4 } from 'uuid';
-
- 
 const schema = z.object({
-  name: z.string().min(4, 'O campo nome não pode ser vazio'),
-  model: z.string().min(1, 'O campo modelo é obrigatório!'),
-  year: z.string().min(1, 'O ano do carro é obrigatório!'),
-  km: z.string().min(1, 'O KM do carro é obrigatório!'),
-  price: z.string().min(1, 'O preço do carro é obrigatório!'),
-  city: z.string().min(1, 'A cidade é obrigatório!'),
-  whatsapp: z.string().min(1, 'O telefone é obrigatório!').refine((value) => /^(\d{10,12})$/.test(value), {
-    message: 'Número de telefone inválido!'
+  name: z.string().min(1,"O campo nome é obrigatório"),
+  model: z.string().min(1,"O modelo é obrigatório"),
+  year: z.string().min(1,"O Ano do carro é obrigatório"),
+  km: z.string().min(1,"O KM do carro é obrigatório"),
+  price: z.string().min(1,"O preço é obrigatório"),
+  city: z.string().min(1,"A cidade é obrigatória"),
+  whatsapp: z.string().min(1, "O Telefone é obrigatório").refine((value) => /^(\d{11,12})$/.test(value), {
+    message: "Numero de telefone invalido."
   }),
-  description: z.string().min(1, 'A descrição do carro é obrigatório!'),
+  description: z.string().min(1,"A descrição é obrigatória")
 })
 
-type FormData = z.infer<typeof schema>
+type FormData = z.infer<typeof schema>;
 
-export function New(){
 
-  const { user } = useContext(AuthContext);
+interface ImageItemProps{
+  uid: string;
+  name: string;
+  previewUrl: string;
+  url: string;
+}
 
-  const { register, handleSubmit, formState: {errors}, reset} = useForm<FormData>({
+export function New() {
+  const { user } = useContext(AuthContext)
+  const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>({
     resolver: zodResolver(schema),
-    mode: 'onChange'
+    mode: "onChange"
   })
 
-  function onSubmit(data: FormData){
-    console.log(data);
-  }
+  const [carImages, setCarImages] = useState<ImageItemProps[]>([])
+
 
   async function handleFile(e: ChangeEvent<HTMLInputElement>){
     if(e.target.files && e.target.files[0]){
-      const image = e.target.files[0];
+      const image = e.target.files[0]
 
       if(image.type === 'image/jpeg' || image.type === 'image/png'){
-        //Enviar para o banco a imagem
-        await handleUpload(image);
+        await handleUpload(image)
       }else{
-        alert('Envie uma imagem jpeg ou png')
+        alert("Envie uma imagem jpeg ou png!")
         return;
       }
+
+
     }
   }
-
+  
   async function handleUpload(image: File){
-
-    //Caso nao tenha um usuario
     if(!user?.uid){
       return;
     }
@@ -68,150 +76,225 @@ export function New(){
     const currentUid = user?.uid;
     const uidImage = uuidV4();
 
-    //Caminho para salvar no banco
-
     const uploadRef = ref(storage, `images/${currentUid}/${uidImage}`)
 
     uploadBytes(uploadRef, image)
     .then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((downloadUrl) => {
-        console.log(downloadUrl);
-      })
+        getDownloadURL(snapshot.ref).then((downloadUrl) => {
+          const imageItem = {
+            name: uidImage,
+            uid: currentUid,
+            previewUrl: URL.createObjectURL(image),
+            url: downloadUrl,
+          }
+
+          setCarImages((images) => [...images, imageItem] )
+
+
+        })
     })
 
   }
 
-    return(
-      <Container>
-        <DashboardHeader/>
+  //Cadastrando um carro
 
-        <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2">
+  function onSubmit(data: FormData){
+    
+    if(carImages.length === 0){
+      alert('Envie alguma imagem deste carro')
+      return;
+    }
 
-          <button className="border-2 w-48 rounded-lg flex items-center justify-center cursor-pointer border-gray-600 h-32 md:w-48">
+    const carListImages = carImages.map( car => {
+      return{
+        uid: car.uid,
+        name: car.name,
+        url: car.url
+      }
+    })
 
-            <div className="position absolute cursor-pointer">
-              <FiUpload size={30} color="#000"/>
-            </div>
+    addDoc(collection(db, "cars"), {
+      name: data.name,
+      model: data.model,
+      whatsapp: data.whatsapp,
+      city: data.city,
+      year: data.year,
+      km: data.km,
+      price: data.price,
+      description: data.description,
+      created: new Date(),
+      owner: user?.name,
+      uid: user?.uid,
+      images: carListImages,
+    })
+    .then(() => {
+      reset();
 
-            <div className="cursor-pointer">
-              <input type="file" accept="image" className="opacity-0 cursor-pointer" 
-              onChange={handleFile}/>
-            </div>
+      //Limpando a lista de imagens
+      setCarImages([]);
+    })
+    .catch((err) => {
+      console.log('Erro ao cadastrar no banco')
+    })
 
-          </button> 
+  }
 
-        </div>
+  async function handleDeleteImage(item: ImageItemProps){
+    const imagePath = `images/${item.uid}/${item.name}`;
 
-        <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2 mt-2">
-          <form 
+    const imageRef = ref(storage, imagePath);
+
+    try{
+      await deleteObject(imageRef)
+      setCarImages(carImages.filter((car) => car.url !== item.url))
+    }catch(err){
+      console.log("ERRO AO DELETAR")
+    }
+
+  }
+
+  return (
+
+    <Container>
+
+      <DashboardHeader/>
+
+      <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2">
+        <button className="border-2 w-48 rounded-lg flex items-center justify-center cursor-pointer border-gray-600 h-32 md:w-48">
+          <div className="absolute cursor-pointer">
+            <FiUpload size={30} color="#000" />
+          </div>
+          <div className="cursor-pointer">
+            <input 
+              type="file" 
+              accept="image/*" 
+              className="opacity-0 cursor-pointer" 
+              onChange={handleFile} 
+            />
+          </div>
+        </button>
+
+        {carImages.map( item => (
+          <div key={item.name} className="w-full h-32 flex items-center justify-center relative">
+            <button className="absolute" onClick={() => handleDeleteImage(item) }>
+              <FiTrash size={28} color="#FFF" />
+            </button>
+            <img
+              src={item.previewUrl}
+              className="rounded-lg w-full h-32 object-cover"
+              alt="Foto do carro"
+            />
+          </div>
+        ))}
+      </div>
+
+      <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2 mt-2">
+        <form
           className="w-full"
-          onSubmit={handleSubmit(onSubmit)}
-          >
-            <div className="mb-3">
-              <p className="mb-2 font-medium">Nome do carro</p>
-              <Input
+          onSubmit={handleSubmit(onSubmit)}  
+        >
+          <div className="mb-3">
+            <p className="mb-2 font-medium">Nome do carro</p>
+            <Input
               type="text"
               register={register}
-              name='name'
+              name="name"
               error={errors.name?.message}
-              placeholder="Ex: Onix 1.0...."
-              />
-            </div>
+              placeholder="Ex: Onix 1.0..."
+            />
+          </div>
 
-            <div className="mb-3">
-              <p className="mb-2 font-medium">Modelo do carro</p>
-              <Input
+          <div className="mb-3">
+            <p className="mb-2 font-medium">Modelo do carro</p>
+            <Input
               type="text"
               register={register}
-              name='model'
+              name="model"
               error={errors.model?.message}
-              placeholder="Ex: 1.0 Flex Plus"
+              placeholder="Ex: 1.0 Flex PLUS MANUAL..."
+            />
+          </div>
+
+          <div className="flex w-full mb-3 flex-row items-center gap-4">
+            <div className="w-full">
+              <p className="mb-2 font-medium">Ano</p>
+              <Input
+                type="text"
+                register={register}
+                name="year"
+                error={errors.year?.message}
+                placeholder="Ex: 2016/2016..."
               />
             </div>
 
-            <div className="flex w-full mb-3 flex-row items-center gap-4"> 
-              <div className="w-full">
-                <p className="mb-2 font-medium">Ano</p>
-                <Input
-                type="text"
-                register={register}
-                name='year'
-                error={errors.year?.message}
-                placeholder="Ex: 2007/2007"
-                />
-              </div>
-
-              <div className="w-full">
-                <p className="mb-2 font-medium">KM rodados</p>
-                <Input
-                type="text"
-                register={register}
-                name='km'
-                error={errors.km?.message}
-                placeholder="Ex: 23.000..."
-                />
-              </div>
-
-            </div>
-
-            <div className="flex w-full mb-3 flex-row items-center gap-4"> 
-              <div className="w-full">
-                <p className="mb-2 font-medium">Telefone / WhatsApp</p>
-                <Input
-                type="text"
-                register={register}
-                name='whatsapp'
-                error={errors.whatsapp?.message}
-                placeholder="Ex: 981610033"
-                />
-              </div>
-
-              <div className="w-full">
-                <p className="mb-2 font-medium">Cidade</p>
-                <Input
-                type="text"
-                register={register}
-                name='city'
-                error={errors.city?.message}
-                placeholder="Ex: São Gonçalo - RJ"
-                />
-              </div>
-
-            </div>
-
-            <div className="mb-3">
-              <p className="mb-2 font-medium">Preço</p>
+            <div className="w-full">
+              <p className="mb-2 font-medium">KM rodados</p>
               <Input
+                type="text"
+                register={register}
+                name="km"
+                error={errors.km?.message}
+                placeholder="Ex: 23.900..."
+              />
+            </div>
+
+          </div>
+
+
+          <div className="flex w-full mb-3 flex-row items-center gap-4">
+            <div className="w-full">
+              <p className="mb-2 font-medium">Telefone / Whatsapp</p>
+              <Input
+                type="text"
+                register={register}
+                name="whatsapp"
+                error={errors.whatsapp?.message}
+                placeholder="Ex: 011999101923..."
+              />
+            </div>
+
+            <div className="w-full">
+              <p className="mb-2 font-medium">Cidade</p>
+              <Input
+                type="text"
+                register={register}
+                name="city"
+                error={errors.city?.message}
+                placeholder="Ex: Campo Grande - MS..."
+              />
+            </div>
+
+          </div>
+
+          <div className="mb-3">
+            <p className="mb-2 font-medium">Preço</p>
+            <Input
               type="text"
               register={register}
-              name='price'
+              name="price"
               error={errors.price?.message}
-              placeholder="Ex: 69.000"
-              />
-            </div>
+              placeholder="Ex: 69.000..."
+            />
+          </div>
 
-            <div className="mb-3">
-
-              <p className="mb-2 font-medium">Descrição</p>
-              <textarea
+          <div className="mb-3">
+            <p className="mb-2 font-medium">Descrição</p>
+            <textarea
               className="border-2 w-full rounded-md h-24 px-2"
-              {...register('description')}
+              {...register("description")}
               name="description"
               id="description"
-              placeholder="Digite a descrição completa sobre o carro"
-              >
-                {errors.description && <p className="mb-1 text-red-500">{errors.description.message}</p>}
-              </textarea>
-            </div>
+              placeholder="Digite a descrição completa sobre o carro..."
+            />
+            {errors.description && <p className="mb-1 text-red-500">{errors.description.message}</p>}
+          </div>
 
-            <button type="submit" className="rounded-md bg-zinc-900 text-white font-medium w-full h-10">
-                Cadastrar
-            </button>
+          <button type="submit" className="w-full rounded-md bg-zinc-900 text-white font-medium h-10">
+            Cadastrar
+          </button>
 
-          </form>
-
-        </div>
-
-      </Container>
-    )
-  }
+        </form>
+      </div>
+    </Container>
+  )
+}
